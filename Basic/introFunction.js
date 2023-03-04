675076e90472f4ec3ed672db687c1113c7b62e9a
NexZoa-Naganandhini <naganandhini.nexzoatw@gmail.com>
2023-03-04 11:08:44 +0530

    metioning datatype in function

--- Basic/introFunction.js
+++ Basic/introFunction.js
@@ -1,5 +1,26 @@
+"use strict";
+exports.__esModule = true;
 var sampleFunction = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
     return console.log(name, email, isPaid);
 };
 sampleFunction("Naganandhini", "naganandhini112@gmail.com");
+// mention data tyoe for return value
+function addTwo(num1, num2) {
+    return num1 + num2;
+}
+console.log(addTwo(3, 5));
+// mentioning data type for map function
+var heros = ["hearo1", "hero2", "hero3", "hero4"];
+heros.map(function (hero) {
+    return "Hello" + hero;
+    // return 1; --> show swiggly lines
+});
+// void type - if the function return nothing
+function consoleError(errorMsg) {
+    console.log("Error Msg", errorMsg);
+}
+// return nothing but throwing error message data type
+function fail(msg) {
+    throw new Error(msg);
+}
--- Basic/introFunction.ts
+++ Basic/introFunction.ts
@@ -4,4 +4,30 @@ let sampleFunction = (name: string, email: string, isPaid: boolean = false) => {
 
 sampleFunction("Naganandhini", "naganandhini112@gmail.com");
 
+// mention data tyoe for return value
+function addTwo(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+console.log(addTwo(3, 5));
+
+// mentioning data type for map function
+
+let heros = ["hearo1", "hero2", "hero3", "hero4"];
+heros.map((hero: string): string => {
+    return "Hello" + hero;
+    // return 1; --> show swiggly lines
+});
+
+// void type - if the function return nothing
+
+function consoleError(errorMsg: string): void {
+    console.log("Error Msg", errorMsg);
+}
+
+// return nothing but throwing error message data type
+
+function fail(msg: string): never {
+    throw new Error(msg);
+}
 export {};
